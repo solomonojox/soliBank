44c35d2e8c8f066e666106d058f4a3dc60d66c10
solomonojox <solomonakpas@gmail.com>
2024-09-19 14:57:19 +0100

    first commit

--- client/src/Pages/ProfileUpdate.js
+++ client/src/Pages/ProfileUpdate.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import MenuBar from '../Components/MenuBar';
 
+import { CgLogOut } from 'react-icons/cg';
+import { IoNotifications } from "react-icons/io5";
 import { FaUserCircle } from "react-icons/fa";
 
 const ProfilePage = () => {
@@ -12,9 +14,11 @@ const ProfilePage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [profileImg, setProfileImg] = useState(null);
+  const [showNotification, setShowNotification] = useState(false)
 
   // const navigate = useNavigate();
   const location = useLocation();
+  const navigate = useNavigate();
 
   const [previewImg, setPreviewImg] = useState(null);
   // useEffect(() => {
@@ -73,8 +77,24 @@ const ProfilePage = () => {
     }
   };
 
+  const handleLogout = (showAlert = true) => {
+    localStorage.removeItem('token');
+    if(showAlert){
+      alert('You have been logged out due to inactivity');
+    }
+    navigate('/login', {replace: true});
+  };
+
+  const handleShowNotification = () => {
+    if (showNotification) {
+      setShowNotification(false)
+    } else{
+      setShowNotification(true)
+    }
+  }
+
   return (
-    <div className="profile-page">
+    <div className="profile-page px-4 ">
       {/* Menu buttons */}
       <MenuBar
         profile={<div className='flex flex-col items-center text-[12px] text-[purple] font-bold '>
@@ -82,9 +102,41 @@ const ProfilePage = () => {
           Me
         </div>}
       />
-      <h2>Update Profile</h2>
+
+      {/* Profile welcome */}
+      <div className='py-4 px-1 flex justify-between items-start md:hidden bg-white ' style={{ position: 'sticky', top: 0 }}>
+        <div className='flex items-center gap-1 md:hidden'>
+          <img src={user.profileImg} alt='profile' className='rounded-full w-[50px] h-[50px] object-cover ' />
+          <div className='w-[200px] '>
+            <h3 className='text-[14px] font-bold '>{user.name}</h3>
+            <p className='text-[12px] my-[-3px] text-[purple] '>account no - {user.accountNumber}</p>
+            <p className='text-[12px] my-[-3px] text-[purple] '>{user.email}</p>
+          </div>
+        </div>
+        <div className='flex items-center gap-3'>
+          <div className='text-[purple] font-bold rounded-lg ' onClick={() => handleLogout(false)}>
+            <CgLogOut className='text-[20px] ' />
+            <p className='text-[6px]'>Logout</p>
+          </div>
+          <div className='relative'>
+            <IoNotifications className='text-[purple] text-[25px] ' onClick={() => handleShowNotification()}/>
+            {/* Notification box */}
+            <div className={`bg-[#f1f1f1] w-[180px] rounded-b-2xl rounded-tl-2xl absolute left-[-160px] top-[30px] p-2 ${showNotification ? 'block' : 'hidden'} shadow-2xl `}> 
+              <ul>
+                <li><p className='text-[12px] border-b py-1 '>Solomon rejected your request</p></li>
+                <li><p className='text-[12px] border-b py-1 '>Solomon rejected your request</p></li>
+                <li><p className='text-[12px] border-b py-1 '>Solomon rejected your request</p></li>
+                <li><p className='text-[12px] border-b py-1 '>Solomon rejected your request</p></li>
+                <li><p className='text-[12px] border-b py-1 '>Solomon rejected your request</p></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <h2 className='text-[25px] font-medium '>Update Profile</h2>
       <form onSubmit={handleSubmit}>
-        <div>
+        <div className='mt-4 flex justify-between border p-2 rounded-lg'>
           <label>Name:</label>
           <input
             type="text"
@@ -95,9 +147,12 @@ const ProfilePage = () => {
         </div>
 
         {/* Display Account Number */}
-        <p>Account Number: {location.state.accountNumber}</p>
+        <div className='mt-4 flex justify-between border p-2 rounded-lg'>
+          <p>Account Number:</p>
+          <span>{location.state.accountNumber}</span>
+        </div>
 
-        <div>
+        <div className='mt-4 flex justify-between border p-2 rounded-lg'>
           <label>Email:</label>
           <input
             type="email"
@@ -108,7 +163,7 @@ const ProfilePage = () => {
           />
         </div>
 
-        <div>
+        <div className='mt-4 flex justify-between border p-2 rounded-lg'>
           <label>Username:</label>
           <input
             type="text"
@@ -118,7 +173,7 @@ const ProfilePage = () => {
           />
         </div>
 
-        <div>
+        <div className='mt-4 flex justify-between border p-2 rounded-lg'>
           <label>Password:</label>
           <input
             type="password"
@@ -128,17 +183,17 @@ const ProfilePage = () => {
           />
         </div>
 
-        <div>
+        <div className='mt-4'>
           <label>Profile Picture:</label>
           <input
             type="file"
             name="profileImg"
             onChange={handleFileChange}
           />
-          <img src={user.profileImg} alt="Profile Preview" width="100" /> 
+          <img src={user.profileImg} alt="Profile Preview" width="30" /> 
         </div>
 
-        <button type="submit">Update Profile</button>
+        <button className='mt-4 bg-[purple] p-2 rounded text-white text-[18px] ' type="submit">Update Profile</button>
       </form>
     </div>
   );
